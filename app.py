e933e4fad30617caca6b76de465ddbebb23ad04f
Adam Ryan <58327211+AdamPatrickRyan@users.noreply.github.com>
2024-04-29 16:42:41 +0100

    Changes
    Remove pickle

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify, abort
 from models import db, bcrypt, User
 from dotenv import load_dotenv
+import pickle
+import pandas as pd
 import googlemaps
 import os
+
+from geocode import geocode_address
+from geocode import get_latitude_from_geocoded_address
+from geocode import get_longitude_from_geocoded_address
 
 load_dotenv() 
 app = Flask(__name__)
@@ -16,8 +22,14 @@ bcrypt.init_app(app)
 gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
 
+@app.route('/', methods=['GET'])
+def homepage():
+    return "<h1>Home Page</h1>"
+
+
 @app.route('/register', methods=['POST'])
 def register():
+    """A route to register users"""
     data = request.get_json()
     if User.query.filter_by(username=data['username']).first() is not None:
         return jsonify({'message': 'Username already exists'}), 400
@@ -38,15 +50,64 @@ def login():
         return jsonify({'message': 'Login successful'}), 200
     return jsonify({'message': 'Invalid email or password'}), 401
 
+
 @app.route('/geocode', methods=['POST'])
 def geocode():
+    """A route to get the geocoded data of an address"""
     try:
-        address = request.json.get('address')
-        if not address:
+        address_json = request.json.get('address')
+
+        if not address_json:
             return jsonify({'error': 'Address is required'}), 400
-        return jsonify({'location': {'lat': 37.4221, 'lng': -122.0841}})
+        
+        geocode_result= geocode_address(gmaps, address_json)
+
+        if not geocode_result:
+            return jsonify({'error': 'Geocoded result could not be acquired'}),400
+        
+        return jsonify(geocode_result), 200
+    
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        median_income=request.json.get("MedInc")
+        house_age=request.json.get("HouseAge")
+        average_rooms=request.json.get("AveRooms")
+        average_bedrooms=request.json.get("AveBedrms")
+        population=request.json.get("Population")
+        average_occupancy=request.json.get("AveOccup")
+        address=request.json.get("address")
+    
+        geocode_result= geocode_address(gmaps, address)
+        latitude=get_latitude_from_geocoded_address(geocode_data=geocode_result)
+        longitude=get_longitude_from_geocoded_address(geocode_data=geocode_result)
+
+        data_dictionary= {
+            "MedInc": median_income,
+            "HouseAge": house_age,
+            "AveRooms": average_rooms,
+            "AveBedrms": average_bedrooms,
+            "Population": population,
+            "AveOccup": average_occupancy,
+            "Latitude":latitude,
+            "Longitude":longitude
+            }
+        
+        prediction_data_df=pd.DataFrame(data_dictionary, index=[0])
+
+        model_file = open("rf_model.pkl",'rb')
+        random_forest_model = pickle.load(model_file)
+        prediction=random_forest_model.predict(prediction_data_df)
+        print(f"Prediction:{prediction}")
+
+        return jsonify(prediction.tolist()[0]), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
